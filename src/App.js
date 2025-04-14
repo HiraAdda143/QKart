262ad345f26a8bfd7a953d77c2484dcf63179152
Hira <hiraaddaria143@gmail.com>
2025-04-14 16:34:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Checkout from "./components/Checkout"
 import Thanks from "./components/Thanks"
 
 export const config = {
-  endpoint: `https://qkart-aqjt.onrender.com/api/v1`,
+  endpoint: `https://qkart-18ek.onrender.com/api/v1`,
 };
 
 function App() {
